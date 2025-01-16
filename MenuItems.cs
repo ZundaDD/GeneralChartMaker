01d4c35c3efd76254f8fae5f72d2eb475d6c4079
Hasekawa Mikan <140880794+ZundaDD@users.noreply.github.com>
2025-01-16 23:04:27 +0800

    Init

---
+++ MenuItems.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ChartMaker
+{
+    public partial class MainWindow : Window
+    {
+        #region 文件菜单
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        #endregion
+
+        #region 编辑菜单
+        private void Undo_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void Redo_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        #endregion
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+
+}
